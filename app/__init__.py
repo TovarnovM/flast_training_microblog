67030b7de5aa35089bb58df7ba38f30347b51f8a
TovarnovM <misha198921rip@gmail.com>
2018-10-20 23:26:11 +0300

    Initial commit, chapter 4 https://habr.com/post/346344/ включительно

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
+
+####
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
